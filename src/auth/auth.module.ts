d10aceb21ec117ce57749d746b6dcdc993c5d3be
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-07-29 12:09:38 +0200

    import modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,20 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth/auth.service';
+import { RolesGuard } from './auth/gards/roles.guard';
+import { JwtAuthGuard } from './auth/gards/jwt-auth.guard';
+import { JwtStrategy } from './auth/gards/jwt.strategy';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
     imports:[
+        forwardRef(()=> UserModule),
+        ConfigModule,
         JwtModule.registerAsync({
             imports:[ConfigModule], 
             inject:[ConfigService],
             useFactory: async (configureService : ConfigService)=> ({
                 secret:configureService.get('JWT_SECRET'),
-                signOptions:{expiresIn :'100s'}
+                signOptions:{expiresIn :'10000s'}
             })
         })
     ],
-    providers: [AuthService],
+    providers: [AuthService , RolesGuard , JwtAuthGuard , JwtStrategy],
     exports:[AuthService]
 })
-export class AuthModule {}
+export class AuthModule {}  
