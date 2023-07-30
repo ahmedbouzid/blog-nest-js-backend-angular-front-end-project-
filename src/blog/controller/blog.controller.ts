a487476794d9346b8318356f806de9c051e50927
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-07-30 16:53:08 +0200

    necessary logic for blog module service && controller

--- src/blog/controller/blog.controller.ts
+++ src/blog/controller/blog.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Request, UseGuards } from '@nestjs/common';
+import { BlogserviceService } from '../blogservice/blogservice.service';
+import { Observable, from } from 'rxjs';
+import { BlogEntry } from '../model/blog-entry.interface';
+import { JwtAuthGuard } from 'src/auth/auth/gards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/auth/gards/roles.guard';
 
-@Controller('blog')
-export class BlogController {}
+@Controller('blogs')
+export class BlogController {
+
+    constructor(private blogService : BlogserviceService) {}
+
+    
+@UseGuards(JwtAuthGuard , RolesGuard)
+@Post()
+create(@Body() blogEntry : BlogEntry , @Request() req) : Observable<BlogEntry> {
+    const user = req.user.user ;
+    return from(this.blogService.create(user ,blogEntry))
+}
+@Get()
+findBlogByEntries (@Query('userId') userId : number) : Observable<BlogEntry[]> {
+
+      if (userId == null) {
+        return this.blogService.findAll();
+    } else {
+        return this.blogService.findByUser(userId) ;
+    }
+}
+@Get (':id')
+findOne(@Param('id') id : number) : Observable<BlogEntry> { 
+    return this.blogService.findOne(id)
+}
+}
