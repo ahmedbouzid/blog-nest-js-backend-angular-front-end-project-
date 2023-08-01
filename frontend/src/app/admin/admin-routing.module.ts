77a26bd9885e7284b9af798415227f54067b9b70
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-08-01 20:07:05 +0200

    -create routing in admin module -  create link of navigation

--- frontend/src/app/admin/admin-routing.module.ts
+++ frontend/src/app/admin/admin-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OverviewComponent } from './components/overview/overview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :'' ,
+  pathMatch :'full',
+  component:OverviewComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
