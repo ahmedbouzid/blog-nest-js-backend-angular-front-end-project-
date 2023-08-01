ea36d06bd7e50f77758f771be7ef3a77fc637310
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-08-01 22:20:20 +0200

    configure login +create registre

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+constructor(private service : AuthService) {}
 
+login() {
+
+this.service.login('ahmed22@gmail.com','123456789').subscribe(
+  data => console.log('Success')
+
+)
+
+  }
 }
