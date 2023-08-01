a11eddcefbec5acb9211f3e135988075261f0cc7
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-08-01 22:19:14 +0200

    add routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistreComponent } from './components/registre/registre.component';
 
 const routes: Routes = [
   {
     path:'admin' ,
     loadChildren:() => import ('./admin/admin.module').then(m => m.AdminModule)
-  }
+  } ,
+  {path:'login' , component :LoginComponent} ,
+  {path:'registre' , component:RegistreComponent}
 ];
 
 @NgModule({
